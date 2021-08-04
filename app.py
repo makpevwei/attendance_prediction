3ad08f5bc432df4ab29adca362189b7154bfe9b5
makpevwei <49917299+makpevwei@users.noreply.github.com>
2021-08-04 11:57:37 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ def results():
     data = request.get_json(force=True)
     prediction = model.predict_proba([np.array(list(data.values()))])[:, 1]
 
-    output =  round(prediction[0], 2)+"%"
-    return jsonify({'prediction': str(output*100)})
+    output =  round(prediction[0], 2)
+    return jsonify({'prediction': str(output*100)+"%"})
 
 if __name__ == "__main__":
     app.run()
