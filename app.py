af6b23e345b18ae9ec1c8ec71338b14665279c66
makpevwei <49917299+makpevwei@users.noreply.github.com>
2021-08-05 00:07:50 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def predict():
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
-    output = round(prediction[0], 2)[:.1]
+    output = round(prediction[0], 2)[:,1]
 
     return render_template("index.html", prediction_text= "PROBABILITY OF BEING ABSENT ; {}".format(output)*100)
 
@@ -28,7 +28,7 @@ def results():
     data = request.get_json(force=True)
     prediction = model.predict([np.array(list(data.values()))])
 
-    output = round(prediction[0],2)[:.1]
+    output = round(prediction[0],2)[:,1]
     return jsonify({'prediction': str(output)*100}+"%")
 
 if __name__ == "__main__":
