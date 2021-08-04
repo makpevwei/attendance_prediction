b13de946726c3613be0532d59ed79e97ff6d738a
makpevwei <49917299+makpevwei@users.noreply.github.com>
2021-08-04 17:08:15 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify, render_template
-from flask import Flask, request, jsonify, render_template
 import pickle
 import numpy as np
 
@@ -17,20 +16,20 @@ def predict():
 
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    prediction = model.predict_proba(final_features)[:, 1]
 
     output = round(prediction[0], 2)
 
-    return render_template("index.html", prediction_text= "PROBABILITY THAT YOUR LOAN WILL BE APPROVED IS ; {}".format(output))
+    return render_template("index.html", prediction_text= "PROBABILITY OF BEING ABSENT IS ; {}".format(output*100))
 
 @app.route('/results',methods=["GET","POST"])
 def results():
 
     data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
+    prediction = model.predict_proba([np.array(list(data.values()))])[:, 1]
 
-    output = prediction[0]
-    return jsonify({'prediction': str(output)})
+    output =  round(prediction[0], 2)+"%"
+    return jsonify({'prediction': str(output*100)})
 
 if __name__ == "__main__":
     app.run()
