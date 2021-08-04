a149be87f6a356a934ca3e22ccec603cb055d151
makpevwei <49917299+makpevwei@users.noreply.github.com>
2021-08-05 00:01:09 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ def predict():
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
-    output = round(prediction[0], 2)
+    output = round(prediction[0], 2)[:.1]
 
-    return render_template("index.html", prediction_text= "PROBABILITY OF BEING ABSENT ; {}".format(output))
+    return render_template("index.html", prediction_text= "PROBABILITY OF BEING ABSENT ; {}".format(output)*100)
 
 @app.route('/results',methods=["GET","POST"])
 def results():
@@ -28,8 +28,8 @@ def results():
     data = request.get_json(force=True)
     prediction = model.predict([np.array(list(data.values()))])
 
-    output = prediction[0]
-    return jsonify({'prediction': str(output)})
+    output = round(prediction[0],2)[:.1]
+    return jsonify({'prediction': str(output)*100}+"%")
 
 if __name__ == "__main__":
     app.run()
--- request.py
+++ request.py
@@ -3,12 +3,11 @@ import json
 
 
 # url = 'http://localhost:5000/results'
-url = 'https://attendance-prediction.herokuapp.com/results'
-r = requests.post(url,json={'Gender':0,'Weather Condition':1,'Service Day':4,
-       'Service Time':2, 'Public Holiday':1, 'SMS Notification':0,
-       'Email Notification':0, 'SMS Reminder':1, 'Email Reminder':0,
-       'Free Transportation':1, 'External Speaker':0, 'Church Venue':1,
-       'Outreach Program':0, 'Celebration':1, 'Love Feast':0})
+url = 'https://attendance-flask.herokuapp.com/results'
+r = requests.post(url,json={'Gender':1,'Weather Condition':1,'Service Day':4,'Service Time':2, 'Public Holiday':1, 'SMS Notification':1,
+       'Email Notification':1, 'SMS Reminder':1, 'Email Reminder':1,
+       'Free Transportation':1, 'External Speaker':1, 'Church Venue':1,
+       'Outreach Program':1,'Love Feast':1})
 
 print("Status code: ", r.status_code)
 
