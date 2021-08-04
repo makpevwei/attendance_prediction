06dbf0b7a6409388311eb0f11533818d23fbeeee
makpevwei <49917299+makpevwei@users.noreply.github.com>
2021-08-04 10:59:02 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+
+
+app = Flask(__name__)
+model = pickle.load(open("model.pkl", "rb"))
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict",methods=["POST"])
+def predict():
+
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict_proba(final_features)[:, 1]
+
+    output = round(prediction[0], 2)
+
+    return render_template("index.html", prediction_text= "PROBABILITY OF BEING ABSENT IS ; {}".format(output))
+
+@app.route('/results',methods=["GET","POST"])
+def results():
+
+    data = request.get_json(force=True)
+    prediction = model.predict_proba([np.array(list(data.values()))])[:, 1]
+
+    output = prediction[0]
+    return jsonify({'prediction': str(output)})
+
+if __name__ == "__main__":
+    app.run()
---
+++ request.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+
+# url = 'http://localhost:5000/results'
+url = 'https://loan-web-app.herokuapp.com/results'
+r = requests.post(url,json={'Gender':0, 'Weather Condition':1, 'Service Day':3,
+       'Service Time':2, 'Public Holiday':1, 'SMS Notification':0,
+       'Email Notification':0, 'SMS Reminder':1, 'Email Reminder':0,
+       'Free Transportation':1, 'External Speaker':0, 'Church Venue':1,
+       'Outreach Program':0, 'Celebration':1, 'Love Feast':0,})
+
+print("Status code: ", r.status_code)
+
+print(r.json())
