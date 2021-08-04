506962945e8810fc7e49b0aab915eeb6180a46d4
makpevwei <49917299+makpevwei@users.noreply.github.com>
2021-08-04 16:59:47 +0100

    Add files via upload

--- request.py
+++ request.py
@@ -8,7 +8,7 @@ r = requests.post(url,json={'Gender':0,'Weather Condition':1,'Service Day':4,
        'Service Time':2, 'Public Holiday':1, 'SMS Notification':0,
        'Email Notification':0, 'SMS Reminder':1, 'Email Reminder':0,
        'Free Transportation':1, 'External Speaker':0, 'Church Venue':1,
-       'Outreach Program':0, 'Celebration':1, 'Love Feast':0,})
+       'Outreach Program':0, 'Celebration':1, 'Love Feast':0})
 
 print("Status code: ", r.status_code)
 
