2522419a222bd0a71d16fbc1a3a754d5b980b49f
makpevwei <49917299+makpevwei@users.noreply.github.com>
2021-08-04 16:20:40 +0100

    Add files via upload

--- request.py
+++ request.py
@@ -4,7 +4,7 @@ import json
 
 # url = 'http://localhost:5000/results'
 url = 'https://attendance-prediction.herokuapp.com/results'
-r = requests.post(url,json={'Gender':0, 'Weather Condition':1, 'Service Day':3,
+r = requests.post(url,json={'Gender':0,'Weather Condition':1,'Service Day':4,
        'Service Time':2, 'Public Holiday':1, 'SMS Notification':0,
        'Email Notification':0, 'SMS Reminder':1, 'Email Reminder':0,
        'Free Transportation':1, 'External Speaker':0, 'Church Venue':1,
